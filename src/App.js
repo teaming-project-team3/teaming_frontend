66a1db3682b7247020d0997eada42ca581d5423c
jsw4215 <jsw4215van@gmail.com>
2022-03-06 03:28:06 +0900

    aws

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      AWS CI/CD 
-    </div>
-  );
+  
+
+  return <div className="App">AWS CI/CD</div>;
 }
 
 export default App;
