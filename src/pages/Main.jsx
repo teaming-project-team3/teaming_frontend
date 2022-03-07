4230855e86ec3c347e58e32580c8f2158c2dd56a
zzerowin <alaykr123@gmail.com>
2022-03-07 20:49:50 +0900

    메인페이지 구현중

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,24 @@
-function Main() {
-  return (
-    <div style={{backgroundColor: "black", height: "200vh"}}>
+import styled from "styled-components";
 
-    </div>
+const Wrapper = styled.div`
+  background-color: whitesmoke;
+  height: 200vh;
+  margin-top: 100px;
+`;
+
+const Title = styled.h1`
+  font-size: 32px;
+  margin-left: 100px;
+`;
+
+function FindProject() {
+
+  return (
+    <Wrapper>
+      <Title>너! 내 동료가 되라👊</Title>
+    </Wrapper>
+
   );
 }
 
-export default Main;
+export default FindProject;
