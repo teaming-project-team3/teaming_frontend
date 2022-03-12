9e59d5320d3ecb582fab3189ee267736abe6f993
jsw4215 <55970155+jsw4215@users.noreply.github.com>
2022-03-12 19:24:08 +0900

    re commit chatt to resolve conflit #47

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import "./App.css";
 import SignUp from "./pages/SignUp";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
@@ -14,19 +15,16 @@ import CreateProject from "./pages/CreateProject"
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import ProjectRoom from "./pages/ProjectRoom";
+// import ProjectRoom from "./pages/ProjectRoom";
 import TempOAuth from "./pages/TempOAuth";
-import DMJoin from "./Components/Organisms/DMJoin/DMJoin";
-import DMChat from "./Components/Organisms/DMChat/DMChat";
-
-
+import UserInfo from "./pages/UserInfo";
 
 function App() {
   return (
     <BrowserRouter>
       {/* <Header /> */}
         <Routes>
-          <Route path="/" element={<ProjectRoom />} />
+          <Route path="/" element={<UserInfo />} />
           <Route path="/oauth" element={<TempOAuth />} />
           <Route path="/main" element={<Main/>} />
           <Route path="/myPage" element={<MyPage/>} />
@@ -38,8 +36,6 @@ function App() {
           <Route path="/signUp" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/auth/kakao/redirect" element={<KakaoRedirect />} />
-          <Route path="/chat/join" element={<DMJoin/>}/>
-          <Route path="/chat/dm" element={<DMChat/>}/>
         </Routes>
     </BrowserRouter>
   );
