bc92f4f539ed998482e15c692f9ca520ee0ac772
jsw4215 <55970155+jsw4215@users.noreply.github.com>
2022-03-18 21:45:35 +0900

    user status page #81

--- src/pages/UserStat.js
+++ src/pages/UserStat.js
@@ -1,13 +1,12 @@
 import * as React from "react";
 import RadarChart from "../Components/Molecules/RadarChart";
-import ProjectCard from "../Components/Organisms/ProjectCard";
-import Grid from "@mui/material/Grid";
-import { styled } from '@mui/material/styles';
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Pagination from "react-js-pagination";
-import _styled from "styled-components";
-
+import Image from "../Components/Atoms/Image";
+import userDummy3 from "../static/images/userStats/example_user.png";
+import FortFolioCard from "../Components/Organisms/FortFolioCard";
+import figma from "../static/images/userStats/figmaLogo.png";
+import SkillBadge from "../Components/Molecules/SkillBadge";
+import GitHubLogo from "../static/images/userStats/gitLogo.png"
+import UrlLink from "../Components/Molecules/UrlLink";
 
 export default function UserStat() {
   const [isLeader, setIsLeader] = React.useState(false);
@@ -18,90 +17,104 @@ export default function UserStat() {
     }
   }, [isLeader]);
 
-  const [page, setPage] = React.useState(1);
-  const handlePageChange = (page) => {
-    setPage(page);
-  };
-
-  const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  }));
-
   return (
     <>
-      <div style={{ display: "flex" }}>
-        <div>
-          <div>유저명 : </div>
+      <div className="bg-[#E5E5E5]">
+        <div className="h-[30vh] bg-[#121414]" />
 
-          <img src="https://ghchart.rshah.org/jamesujeon" alt="" />
-          {/* <img src="https://ghchart.rshah.org/219138/jamesujeon"/> */}
-
-          <div style={{ display: "flex" }}>
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-          </div>
-
-          <div>
-            프로젝트 횟수 Github URL Tech Blog BOJ Tier Programmers Completed
-            Projects Skills Special Stack Education Github repository Github
-            commit Projects With Teaming #1 ~~ #2 ~~ Projects Involved : #1
-            Projects completed : #2 Projects Abandonment : #1
-          </div>
+        <div className="flex justify-center w-screen mt-[-4.063rem]">
+          <Image shape="circle" src={userDummy3} size={"130"}></Image>
         </div>
-        <div>
-          <div style={{ width: "300px" }} display="flex">
-            <RadarChart curr={"userA"}></RadarChart>
-          </div>
+        <text className="flex justify-center mt-8 text-4xl font-notoB text-[#121414]">
+          이도윤
+        </text>
+        <text className="flex justify-center mt-4 text-[1.375rem] font-noto2 text-[#71797D]">
+          Ux Designer
+        </text>
 
-          <div>
-            사용 언어 통계
-            {/* github-readme-stats.vercel.app/api/top-langs/?username={사용자명}&langs_count=8 */}
-          </div>
-
-          <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={3}>
-                <Item>스킬창</Item>
-              </Grid>
-              <Grid item xs={3}>
-                <Item>xs=3</Item>
-              </Grid>
-              <Grid item xs={3}>
-                <Item>xs=3</Item>
-              </Grid>
-              <Grid item xs={3}>
-                <Item>xs=3</Item>
-              </Grid>
-              <Grid item xs={4}>
-                <Item>xs=4</Item>
-              </Grid>
-              <Grid item xs={8}>
-                <Item>xs=8</Item>
-              </Grid>
-            </Grid>
-          </Box>
+        <div className="flex justify-center">
+          <text className="flex content-center mt-8 w-[80vh] text-[0.875rem] font-noto2 text-[#71797D] whitespace-pre-wrap">
+            안녕하세요 :) 늘 새로운 도전으로 예술을 그려내는 디자이너
+            이도윤입니다. 👋🏻 디자인 뿐만 아니라 개발자와 협업하며 다양한 지식을
+            얻어가고 , 도전에 대한 결과물 뿐만 아니라 사람을 알고 함께
+            프로젝트를 진행하고 싶어요!
+          </text>
         </div>
 
-        <PaginationHorizontal>
-          <Pagination
-            activePage={page}
-            itemsCountPerPage={10}
-            totalItemsCount={50}
-            pageRangeDisplayed={5}
-            prevPageText="‹"
-            nextPageText="›"
-            onChange={handlePageChange}
-          ></Pagination>
-        </PaginationHorizontal>
+        <div className="flex justify-center w-screen mt-[4.313rem]">
+          <div className="bg-white h-fit w-[12.813rem] mr-7 p-4 box-border rounded-[0.625rem]">
+            <div className="bg-slate-200 rounded-[0.625rem] p-2.5 font-notoB text-gray-900 text-sm mb-3">
+            포트폴리오
+            </div>
+            <div className="rounded-[0.625rem] p-2.5 font-noto2 text-gray-900 text-sm mb-3">
+            전문분야
+            </div>
+            <div className="rounded-[0.625rem] p-2.5 font-noto2 text-gray-900 text-sm">
+            URL
+            </div>
+          </div>
+
+          {/* 포트폴리오 블럭 */}
+          <div className="h-[67.625rem] w-[54.688rem] bg-white box-border rounded-[0.625rem]">
+            <div className="text-xl font-bold font-noto2 mt-7 ml-[1.8rem] pl-[1.8rem] pb-6 border-b-2 border-gray-900">
+              포트폴리오
+            </div>
+            
+            {/* 깃헙 잔디 */}
+            <div className="flex ml-[3.6rem] mt-7 border-2 mr-[6.875rem]">
+              <img src="https://ghchart.rshah.org/jamesujeon" alt="" />
+            </div>
+            
+            <FortFolioCard></FortFolioCard>
+            <FortFolioCard></FortFolioCard>
+            <FortFolioCard></FortFolioCard>
+            <FortFolioCard></FortFolioCard>
+            
+          </div>
+        </div>
+
+        <div className="flex justify-center w-screen">
+          <div className="mt-[2.188rem] ml-[14.5rem] h-[26.438rem] w-[54.688rem] bg-white box-border rounded-[0.625rem]">
+            <div className="text-xl font-bold font-noto2 mt-7 ml-[1.8rem] pl-[1.8rem] pb-6 border-b-2 border-gray-900">
+              전문분야
+            </div>
+
+            <div className="flex mt-[3.75rem] ml-[1.8rem]">
+
+            <div className="flex flex-wrap w-3/5">
+
+              <SkillBadge src={figma} name={"Figma"}></SkillBadge>
+              <SkillBadge src={figma} name={"Figma"}></SkillBadge>
+              <SkillBadge src={figma} name={"Figma"}></SkillBadge>
+              <SkillBadge src={figma} name={"Figma"}></SkillBadge>
+              <SkillBadge src={figma} name={"Figma"}></SkillBadge>
+
+            </div>
+
+              <div className="h-[15.938rem] w-[15.938rem]">
+                <RadarChart curr={"userA"}></RadarChart>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-start">
+          <div className="mt-[2.188rem] ml-[23.5rem] mb-[2.188rem] h-[26.438rem] w-[27rem] bg-white box-border rounded-[0.625rem]">
+            
+            <div className="text-xl font-bold font-noto2 mt-7 ml-[1.8rem] pl-[1.8rem] pb-6 border-b-2 border-gray-900">
+              URL
+            </div>
+
+
+            <UrlLink logo={GitHubLogo} url={"https://Behance.com/heeyeon9578"}></UrlLink>
+
+            <UrlLink logo={GitHubLogo} url={"https://Behance.com/heeyeon9578"}></UrlLink>
+
+            <UrlLink logo={GitHubLogo} url={"https://Behance.com/heeyeon9578"}></UrlLink>
+
+          </div>
+        </div>
       </div>
     </>
   );
 }
-
-const PaginationHorizontal = _styled.div`& ul li {list-style-type: none; float: left; margin-right:4px;}`
-
