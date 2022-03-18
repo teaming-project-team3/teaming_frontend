8c19d82de782d90b218884230097c70be76c4d3e
zzerowin <alaykr123@gmail.com>
2022-03-18 17:48:49 +0900

    #80 메인페이지 디자인 적용 중2

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import styled from "styled-components";
 import Modal from "../Components/Modals/Modal";
 import BannerImg from "../static/BannerImg.png";
+import RightCursor from "../static/RightCursor.png";
+import Pic from "../static/Pic.png"
+import Pic2 from "../static/Pic2.png"
 
 const Wrap = styled.div`
   width: 100%;
@@ -83,6 +86,83 @@ const SubBanner = styled.div`
   border-radius: 12px;
 `
 
+const SubBannerTitle = styled.div`
+  margin-right: 48px;
+  margin-top: 28px;
+  margin-bottom: 76px;
+  width: 160px;
+  height: 64px;
+  font-family: 'Noto Sans CJK KR';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20.3691px;
+  line-height: 150%;
+  color: #FFFFFF;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`
+
+const SubBannerBtn = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px 16px;
+  position: absolute;
+  width: 110.73px;
+  height: 38px;
+  left: 0px;
+  top: 74px;
+  background: #FFFFFF;
+  border-radius: 50px;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+  margin: 24px 32px;
+`
+
+const SubBannerBtnTitle = styled.div`
+  position: static;
+  width: 45px;
+  height: 18px;
+  left: 16px;
+  top: 10px;
+  font-family: 'Noto Sans CJK KR';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 150%;
+  color: #000000;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  margin: auto;
+`
+
+const Cursor = styled.div`
+  //background-image: url(${RightCursor});
+  background-repeat: no-repeat;
+  position: absolute;
+  width: 13.73px;
+  height: 12.02px;
+  border: 0px solid #000000;
+  margin: 0px 65px;
+`
+
+const CharImg = styled.div`
+  background-image: url(${Pic});
+  background-repeat: no-repeat;
+  position: static;
+  width: 166px;
+  height: 128px;
+  left: 326px;
+  top: 20px;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+  margin: 0px 50px;
+`
+
 const SubBanner2 = styled.div`
   margin-left: 166px;
   display: flex;
@@ -99,6 +179,104 @@ const SubBanner2 = styled.div`
   border-radius: 12px;
 `
 
+// const SubBannerTitle = styled.div`
+//   margin-right: 48px;
+//   margin-top: 28px;
+//   margin-bottom: 76px;
+//   width: 160px;
+//   height: 64px;
+//   font-family: 'Noto Sans CJK KR';
+//   font-style: normal;
+//   font-weight: 700;
+//   font-size: 20.3691px;
+//   line-height: 150%;
+//   color: #FFFFFF;
+//   flex: none;
+//   order: 0;
+//   flex-grow: 0;
+// `
+
+const SubBannerTitle2 = styled.div`
+  margin-left: 50px;
+  position: static;
+  width: 250px;
+  height: 64px;
+  left: 0px;
+  top: 0px;
+  font-family: 'Noto Sans CJK KR';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20.3691px;
+  line-height: 150%;
+  color: #FFFFFF;
+  flex: none;
+  order: 0;
+  align-self: stretch;
+  flex-grow: 0;
+`
+
+const SubBannerBtn2 = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px 16px;
+  position: absolute;
+  width: 110.73px;
+  height: 38px;
+  left: 0px;
+  top: 74px;
+  background: #FFFFFF;
+  border-radius: 50px;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+  margin: 24px 32px;
+`
+
+const SubBannerBtnTitle2 = styled.div`
+  position: static;
+  width: 45px;
+  height: 18px;
+  left: 16px;
+  top: 10px;
+  font-family: 'Noto Sans CJK KR';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 150%;
+  color: #000000;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  margin: auto;
+`
+
+const Cursor2 = styled.div`
+  //background-image: url(${RightCursor});
+  background-repeat: no-repeat;
+  position: absolute;
+  width: 13.73px;
+  height: 12.02px;
+  border: 0px solid #000000;
+  margin: 0px 65px;
+`
+
+const CharImg2 = styled.div`
+  background-image: url(${Pic2});
+  background-repeat: no-repeat;
+  position: static;
+  width: 166px;
+  height: 128px;
+  left: 326px;
+  top: 20px;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+  margin: 0px 50px;
+`
+
+
+
 
 function Main() {
   return (
@@ -112,10 +290,14 @@ function Main() {
           </Banner>
           <div>
             <SubBanner>
-
+              <SubBannerTitle>사이드 프로젝트를 찾는 분이라면?</SubBannerTitle>
+              <SubBannerBtn><SubBannerBtnTitle>바로가기</SubBannerBtnTitle><Cursor /> </SubBannerBtn>
+              <CharImg />
             </SubBanner>
             <SubBanner2>
-
+              <SubBannerTitle2>사이드 프로젝트 아이디어가 있으신 분이라면?</SubBannerTitle2>
+              <SubBannerBtn2><SubBannerBtnTitle2>바로가기</SubBannerBtnTitle2><Cursor /> </SubBannerBtn2>
+              <CharImg2 />
             </SubBanner2>
           </div>
 
